66db7b7eeb510587bf3569d7a98ea8fd4dfc2588
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-11-02 00:47:10 +1100

    Add check events method

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -29,10 +29,7 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self.check_events()
 
             # Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color)
@@ -41,7 +38,14 @@ class AlienInvasion:
 
             # Update the surface to the drawn screen.
             pygame.display.flip()
-    
+
+    def check_events(self):
+        """Respond to keypresses and mouse events."""
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
 if __name__ == '__main__':
     # Make a game instance and run the game.
     ai = AlienInvasion()
