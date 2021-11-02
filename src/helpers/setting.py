de2edc8bc45b6b236d3661aab56ae3ad9199f293
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-11-03 00:01:00 +1100

    Update the settings.

--- src/helpers/setting.py
+++ src/helpers/setting.py
@@ -8,9 +8,10 @@ class Settings:
         self.bg_color = (0, 0, 0)
 
         # Ships settings
-        # When the ship moves, 3 pixels / pass
         self.ship_speed = 6
         self.ship_limit = 3
+        self.ship_increase_hp = 0.5
+        self.ship_increase_speed = 1
 
         # Bullet settings
         self.bullet_speed = 5.0
@@ -18,9 +19,14 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (200, 60, 60)
         self.bullet_stored = 10
-    
+        self.bullet_increase_width = 2
+        self.bullet_increase_store = 1
+        self.bullet_increase_speed = 1.0
+
         # Ufo settings
         self.ufo_speed = 2
-        self.fleet_drop_speed = 20
+        self.fleet_drop_speed = 5
+        self.ufo_increase_speed = self.ufo_speed * 1.02
+        self.fleet_increase_drop_speed = self.fleet_drop_speed * 1.02
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
